48ccc11d1c78a5ea28ba7e445793f5823766a8f2
Sheng Zhong <zhsh@umich.edu>
2023-01-14 20:37:02 -0500

    Generate medial balls from command line input files

--- example_usage.cpp
+++ example_usage.cpp
@@ -1,32 +1,95 @@
-#include <stdio.h>
 #include <math.h>
-#include <vector>
+#include <stdio.h>
 
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
 
 #include "minimal_free_space_constraints.h"
 
-int main()
-{
-    // Generate a point cloud of a circle, with normals pointing
-    // inward to the center. Noise is added to the radius.
-    std::vector<Point> points;
-    for (int i = 0; i < 250; i++)
-    {
-        float t = 2.0f*3.14f*(i+0.5f)/250.0f;
-        float r = 1.0f + 0.2f*(rand() % 10000)/10000.0f;
-        float nx = cosf(t);
-        float ny = sinf(t);
-        float x = r*nx;
-        float y = r*ny;
+using Points = std::vector<Point>;
+using Balls = std::vector<Ball>;
+using PointsPerPoke = std::map<int, Points>;
 
-        Point pt = {};
-        pt.jump = false;
-        pt.p = {x, y, 0.0f};
-        pt.n = {-nx, -ny, 0.0f};
-        points.push_back(pt);
+PointsPerPoke read_freespace_pc(std::ifstream &input) {
+    PointsPerPoke ret;
+    while (input) {
+        int num_pokes = -1, num_points = -1;
+        input >> num_pokes >> num_points;
+        if (num_pokes == -1) {
+            break;
+        }
+
+        std::cout << "freespace: poke " << num_pokes << " with " << num_points
+                  << " points\n";
+        Points pts;
+
+        for (int i = 0; i < num_points; i++) {
+            float x, y, z;
+            input >> x >> y >> z;
+
+            Point pt = {};
+            // freespace boundary is not necessarily part of physical surfaces
+            pt.jump = true;
+            pt.p = {x, y, z};
+            pts.push_back(pt);
+        }
+        for (int i = 0; i < num_points; i++) {
+            float x, y, z;
+            input >> x >> y >> z;
+            pts[i].n = {x, y, z};
+        }
+        ret[num_pokes] = pts;
     }
 
-    float r_max = 10.0f;
+    return ret;
+}
+
+void read_contact_pc(std::ifstream &input, PointsPerPoke& points_per_poke) {
+    while (input) {
+        int num_pokes = -1, num_points = -1;
+        input >> num_pokes >> num_points;
+        if (num_pokes == -1) {
+            break;
+        }
+
+        std::cout << "contacts: poke " << num_pokes << " with " << num_points
+                  << " points\n";
+
+        auto& pts = points_per_poke[num_pokes];
+
+        for (int i = 0; i < num_points; i++) {
+            float x, y, z;
+            int in_contact;
+            input >> x >> y >> z >> in_contact;
+            if (in_contact != 1) {
+                continue;
+            }
+
+            Point pt = {};
+            // contact points are known to be on physical surfaces
+            pt.jump = false;
+            pt.p = {x, y, z};
+            // these experiments are always pushing along +x, so estimate normal of -x
+            pt.n = {-1, 0, 0};
+            pts.push_back(pt);
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+    std::string freespace_file(argv[1]);
+    std::string contact_file(argv[2]);
+    std::string output_file(argv[3]);
+
+    std::ifstream freespace_stream{freespace_file};
+    auto points_per_poke = read_freespace_pc(freespace_stream);
+    std::ifstream contact_stream{contact_file};
+    read_contact_pc(contact_stream, points_per_poke);
+
+    float r_max = 1.0f;
     float min_angle = 0.1f;
     int smoothing_neighborhood_size = 4;
     float smoothing_factor = 0.1f;
@@ -48,42 +111,52 @@ int main()
     //     min-cover simplification to use a given number of
     //     balls. Set to 0 to only use provided ball_delta.
 
-    compute_medial_axis_distance(points,
-        r_max,
-        min_angle,
-        smoothing_neighborhood_size,
-        smoothing_factor);
+    std::ofstream output_stream(output_file);
 
-    std::vector<Ball> balls;
-    for (auto pt : points)
-    {
-        Ball ball;
-        ball.c = pt.p + pt.mad*pt.n;
-        ball.p = pt.p;
-        ball.r = pt.mad;
-        balls.push_back(ball);
-    }
+    for (auto& kv: points_per_poke) {
+        int num_pokes = kv.first;
+        Points& points = kv.second;
 
-    Cover cover;
-    cover.initialize(&balls[0], (int)balls.size());
-    cover.update_min_cover(ball_delta, max_balls_to_use);
+        std::cout << "compute ball: poke " << num_pokes << " with " << points.size() << " points" << std::endl;
+        compute_medial_axis_distance(points, r_max, min_angle,
+                                    smoothing_neighborhood_size, smoothing_factor);
 
-    // cover.is_used[i] will now be true if ball i is part
-    // of the simplified cover of free space.
-
-    // cover.update_min_cover can be called repeatedly after
-    // this point with a smaller ball_delta for fast updates.
-    // If called with a larger ball_delta, it will be slower.
-
-    // Save the balls for visualization in visualize.py
-    {
-        FILE *file = fopen("balls.txt", "w");
-        for (size_t i = 0; i < balls.size(); i++)
-        {
-            Ball b = balls[i];
-            if (cover.is_used[i])
-                fprintf(file, "%g %g %g %g %g %g %g\n", b.c.x, b.c.y, b.c.z, b.p.x, b.p.y, b.p.z, b.r);
+        Balls balls;
+        for (auto pt : points) {
+            Ball ball;
+            ball.c = pt.p + pt.mad * pt.n;
+            ball.p = pt.p;
+            ball.r = pt.mad;
+            balls.push_back(ball);
         }
-        fclose(file);
+
+        // cover.is_used[i] will now be true if ball i is part
+        // of the simplified cover of free space.
+
+        // cover.update_min_cover can be called repeatedly after
+        // this point with a smaller ball_delta for fast updates.
+        // If called with a larger ball_delta, it will be slower.
+        Cover cover;
+        cover.initialize(&balls[0], (int)balls.size());
+        cover.update_min_cover(ball_delta, max_balls_to_use);
+
+        size_t balls_used = 0;
+        for (size_t i = 0; i < balls.size(); i++) {
+            const auto& b = balls[i];
+            if (cover.is_used[i]) {
+                balls_used += 1;
+            }
+        }
+
+        std::cout << "poke " << num_pokes << " with " << balls_used << " balls used" << std::endl;
+        output_stream << num_pokes << " " << balls_used << std::endl;
+
+        for (size_t i = 0; i < balls.size(); i++) {
+            const auto& b = balls[i];
+            if (cover.is_used[i]) {
+                output_stream << b.c.x << ' ' << b.c.y << ' ' << b.c.z << ' ' <<
+                        b.p.x << ' ' << b.p.y << ' ' << b.p.z << ' ' << b.r << std::endl;
+            }
+        }
     }
 }
